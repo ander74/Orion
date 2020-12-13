fd2f8741b953736a8a6233e4a1338677776cda89
Ander <ander74@gmail.com>
2020-12-13 16:30:35 +0100

    Actualización Barras de desplazamiento en opciones.

---
+++ Orion/Convertidores/ConvertidorVisibilityScrollBar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Orion.Convertidores {
+
+    public class ConvertidorVisibilityScrollBar : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (value is bool valor) {
+                return valor ? ScrollBarVisibility.Visible : ScrollBarVisibility.Hidden;
+            }
+            return ScrollBarVisibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
